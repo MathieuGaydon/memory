ae5c08741ad67ae2e33cfaec5b861d2431f15477
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-05-20 17:22:12 +0200

    fix(security): add noopener noreferrer to external links

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
