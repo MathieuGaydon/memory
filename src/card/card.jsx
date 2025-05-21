d86d228411c99ec7725372ea4f35af1d317aea83
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-05-21 10:49:41 +0200

    feat(card): delete logic card

--- src/card/card.jsx
+++ src/card/card.jsx
@@ -1,42 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import './card.css';
 
-
-const CardVerso = [
-    { id: 1, img: "../public/card_verso/kraken.png", value: 10 },
-    { id: 2, img: "../public/card_verso/Sirene.png", value: 20 },
-    { id: 3, img: "../public/card_verso/leviathan.png", value: 30 },
-    { id: 4, img: "../public/card_verso/lockhness.png", value: 40 },
-    { id: 5, img: "../public/card_verso/jormungand.png", value: 50 },
-    { id: 6, img: "../public/card_verso/dragon.png", value: 60 },
-    { id: 7, img: "../public/card_verso/hydre.png", value: 70 },
-    { id: 8, img: "../public/card_verso/Hippocampe.png", value: 80 }
-];
-
-const CardRecto = "../public/card_recto/card_recto.png";
-
-function Card(props) {
+function Card({ index, cardData, cardRecto, onCardFlip }) {
     const [isFlipped, setIsFlipped] = useState(false);
-    const [selectedCard, setSelectedCard] = useState(null);
-    
-    // Sélectionne une carte aléatoire au chargement du composant
-    useEffect(() => {
-        selectRandomCard();
-    }, []);
-    
-    const selectRandomCard = () => {
-        const randomIndex = Math.floor(Math.random() * CardVerso.length);
-        setSelectedCard(CardVerso[randomIndex]);
-    };
 
     const handleClick = () => {
-        setIsFlipped(!isFlipped);
-        
-        console.log("clique effectué");
-        
-
-        if (!isFlipped && selectedCard && props.onCardFlip) {
-            props.onCardFlip(selectedCard.id, selectedCard.value);
+        if (!isFlipped) {
+            setIsFlipped(true);
+            onCardFlip(index);
         }
     };
     
@@ -48,18 +19,14 @@ function Card(props) {
             >
                 {/* Face avant (recto) */}
                 <div className="card-face card-front">
-                    <img src={CardRecto} alt="Recto" />
+                    <img src={cardRecto} alt="Recto" />
                 </div>
                 
-
+                {/* Face arrière (verso) */}
                 <div className="card-face card-back">
-                    {selectedCard && (
-                        <>
-                            <img src={selectedCard.img} alt={`Carte ${selectedCard.id}`} />
-                            {/* Si vous voulez afficher la valeur sur la carte, décommentez ci-dessous */}
-                            {/* <div className="card-value">{selectedCard.value}</div> */}
-                        </>
-                    )}
+                    <img src={cardData.img} alt={`Carte ${cardData.id}`} />
+                    {/* Si vous voulez afficher la valeur sur la carte, décommentez ci-dessous */}
+                    {/* <div className="card-value">{cardData.value}</div> */}
                 </div>
             </div>
         </div>
