dc507926f9d871bf66c21078e3100212123038c1
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-05-23 22:58:18 +0200

    fix(button): add prop-types validation and correct React import

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import '../directive.css';
+import PropTypes from 'prop-types';
 
 const Button = ({ onClick, children, className }) => {
   return (
@@ -9,6 +11,12 @@ const Button = ({ onClick, children, className }) => {
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
 export default Button;
